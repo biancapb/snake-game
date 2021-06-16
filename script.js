584cd36bf6f57e39296e96557f51abf29ab24a13
Bianca Barbosa <yume_shiro@hotmail.com>
2021-06-15 22:13:15 -0300

    creating food

--- script.js
+++ script.js
@@ -8,6 +8,10 @@ snake[0] = {
 }
 
 let direction = "right";
+let food = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
+}
 
 function criarBG() {
     context.fillStyle = "lightgreen";
@@ -21,6 +25,11 @@ function criarCobrinha() {
     }
 }
 
+function drawFood() {
+    context.fillStyle = "red";
+    context.fillRect(food.x, food.y, box, box);
+}
+
 document.addEventListener('keydown', update);
 
 function update(event) {
@@ -31,6 +40,7 @@ function update(event) {
 
 }
 
+
 function iniciarJogo() {
 
     if (snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
@@ -40,6 +50,7 @@ function iniciarJogo() {
 
     criarBG();
     criarCobrinha();
+    drawFood();
 
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
